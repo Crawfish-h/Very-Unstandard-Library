0f190c0c67ac521a36cfbe673f205016d492761e
Crawfish-h <jaidendcrawford@gmail.com>
2025-02-18 15:32:03 -0500

    Fixed TLinked_List Multi and Add functions.

--- Containers/TLinked_List.c
+++ Containers/TLinked_List.c
@@ -40,12 +40,12 @@ TLinked_List* TLinked_List_Init(size_t type_Count, size_t value_Count, ...)
 
     if (value_Count - type_Count > 0)
     {
-        TLinked_List_Add(list, 0, 1, va_arg(va_Args, TGeneric*));
+        TLinked_List_Add(list, 0, va_arg(va_Args, TGeneric*));
     }
 
     for (size_t i = type_Count + 1; i < value_Count; i++)
     {
-        TLinked_List_Add(list, super->Size - 1, 1, va_arg(va_Args, TGeneric*));
+        TLinked_List_Multi(list, 1, 1, va_arg(va_Args, TGeneric*));
     }
 
     va_end(va_Args);
@@ -55,79 +55,28 @@ TLinked_List* TLinked_List_Init(size_t type_Count, size_t value_Count, ...)
 TNode* TLinked_List_Get_Node(TLinked_List* list, ssize_t index)
 {
     TNode* current_node = list->First;
+    TContainer* super = &list->Super;
+
     for (size_t j = 0; j < index; j++)
     {
         if (current_node == NULL)
         {
-            perror("ERORR: invalid index for TLinked_List_Get_Node(...)");
+            fprintf(stderr, "ERORR: invalid index for TLinked_List_Get_Node(...). Index argument: %lld. TLinked_List size: %zu.\n", index, super->Size);
             exit(EXIT_FAILURE);
         }
 
-        if (j != index)
-        {
-            current_node = current_node->Next;
-        }
+        current_node = current_node->Next;
     }
 
     return current_node;
 }
 
-bool TLinked_List_Add(TLinked_List* list, ssize_t index, size_t value_Count, ...)
+bool TLinked_List_Add(TLinked_List* list, ssize_t index, TGeneric* node_Value)
 {
-    va_list va_Args;
-    va_start(va_Args, value_Count);
-    TContainer* super = &list->Super;
-    bool did_Allocate = false;
-
-    if (index < 0)
-    {
-        index = super->Size + index;
-    }
-    
-    if ((index > super->Size - 1 || index < 0))
-    {
-        fprintf(stderr, "ERROR: TLinked_List index out of range. Index argument: %lld. TLinked_List size: %zu.\n", index, super->Size);
-        exit(EXIT_FAILURE);
-    }
-
-    size_t node_Count = 0;
-    if (super->Size == 0)
-    {
-        TNode* first_Node = super->Allocator.Calloc(1, sizeof(TNode));
-        list->First = first_Node;
-        list->Last = first_Node;
-        first_Node->Value = *va_arg(va_Args, TGeneric*);
-        super->Size++;
-        node_Count++;
-        printf("added node val type: %s\n", first_Node->Value.Rtti_.Type.Str);
-        va_end(va_Args);
-        return did_Allocate;
-    }
-
-    for (; node_Count < value_Count; node_Count++)
-    {
-        TNode* new_Node = super->Allocator.Calloc(1, sizeof(TNode));
-        TNode* indexed_Node = TLinked_List_Get_Node(list, index);
-        TNode* old_Next = indexed_Node->Next;
-        indexed_Node->Next = new_Node;
-        new_Node->Next = old_Next;
-        new_Node->Value = *va_arg(va_Args, TGeneric*);
-        
-        printf("added node val type: %s\n", new_Node->Value.Rtti_.Type.Str);
-
-        if (index == super->Size - 1)
-        {
-            list->Last = new_Node;
-        }
-
-        super->Size++;
-    }
-
-    va_end(va_Args);
-    return did_Allocate;
+    TLinked_List_Multi(list, index, 1, node_Value);
 }
 
-bool TLinked_List_Add_Before(TLinked_List* list, ssize_t index, size_t value_Count, ...)
+bool TLinked_List_Multi(TLinked_List* list, ssize_t index, size_t value_Count, ...)
 {
     va_list va_Args;
     va_start(va_Args, value_Count);
@@ -136,44 +85,44 @@ bool TLinked_List_Add_Before(TLinked_List* list, ssize_t index, size_t value_Cou
 
     if (index < 0)
     {
-        index = super->Size - index;
-    }
-
-    if (index > super->Size - 1 || index < 0)
-    {
-        fprintf(stderr, "ERROR: TLinked_List index out of range. Index argument: %lld. TLinked_List size: %zu.\n", index, super->Size);
-        exit(EXIT_FAILURE);
+        index = super->Size + 1 + index;
     }
 
     size_t node_Count = 0;
     if (super->Size == 0)
     {
         TNode* first_Node = super->Allocator.Calloc(1, sizeof(TNode));
+        first_Node->Value = *va_arg(va_Args, TGeneric*);
+        node_Count++;
+        super->Size++;
         list->First = first_Node;
         list->Last = first_Node;
-        first_Node->Value = *va_arg(va_Args, TGeneric*);
-        super->Size++;
-        node_Count++;
-        printf("added node val type: %s\n", first_Node->Value.Rtti_.Type.Str);
-        va_end(va_Args);
-        return did_Allocate;
     }
 
     for (; node_Count < value_Count; node_Count++)
     {
         TNode* new_Node = super->Allocator.Calloc(1, sizeof(TNode));
-        TNode* indexed_Node = TLinked_List_Get_Node(list, index);
-        TNode* old_Next = indexed_Node->Next;
-        indexed_Node->Next = new_Node;
-        new_Node->Next = old_Next;
         new_Node->Value = *va_arg(va_Args, TGeneric*);
-
-        if (index == super->Size - 1)
-        {
-            list->Last = new_Node;
-        }
-        
         super->Size++;
+        ssize_t node_Index = index + node_Count;
+
+        TNode* indexed_Node = TLinked_List_Get_Node(list, node_Index);
+        if (node_Index == super->Size)
+        {
+            indexed_Node->Next = new_Node;
+            list->Last = new_Node;
+        }else
+        {
+            new_Node->Next = indexed_Node;
+            if (node_Index == 0)
+            {
+                list->First = new_Node;
+            }else
+            {
+                TNode* prev_Node = TLinked_List_Get_Node(list, node_Index - 1);
+                prev_Node->Next = new_Node;
+            }
+        }
     }
 
     va_end(va_Args);
--- Containers/TLinked_List.h
+++ Containers/TLinked_List.h
@@ -15,13 +15,15 @@ typedef struct TLinked_List TLinked_List;
 // all types need to come before the values.
 TLinked_List* TLinked_List_Init(size_t type_Count, size_t value_Count, ...);
 
-// Adds nodes that hold the supplied values >AFTER< the [index] argument and returns true if the new value was allocated.
-// Only way to set [TLinked_List.Last].
-bool TLinked_List_Add(TLinked_List* list, ssize_t index, size_t value_Count, ...);
+// Adds a node that holds the supplied values >AFTER< the [index] argument and returns true if the new value was allocated.
+// if [index] equals [TLinked_List.Super.Size - 1] then [TLinked_List.Last] will equal the new node.
+bool TLinked_List_Add(TLinked_List* list, ssize_t index, TGeneric* node_Value);
 
-// Adds nodes that hold the supplied values >BEFORE< the [index] argument and returns true if the new value was allocated.
-// Only way to set [TLinked_List.First].
-bool TLinked_List_Add_Before(TLinked_List* list, ssize_t index, size_t value_Count, ...);
+// Adds multiple nodes that hold the supplied values >AFTER< the [index] argument and returns true if the new value was allocated.
+// if [index] equals [TLinked_List.Super.Size - 1] then [TLinked_List.Last] will equal the last added node.
+bool TLinked_List_Multi(TLinked_List* list, ssize_t index, size_t value_Count, ...);
+
+
 
 // Returns the data at [index].
 void* TLinked_List_Get(TLinked_List* list, ssize_t index);
